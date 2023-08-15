f81041adc71d2cf17f78ad98a2f372fbac73dfc1
Avi Mamenko <avimamenko@gmail.com>
2023-08-15 02:32:00 -0400

    Updated yt-dlp extract-audio Python command

--- python_scripts/yt-dlp.py
+++ python_scripts/yt-dlp.py
@@ -1,4 +1,4 @@
 import os
 import sys
 
-os.system('yt-dlp "{}" --extract-audio --audio-format mp3 -o "{}"'.format(sys.argv[1], sys.argv[2]))
+os.system('yt-dlp "{}" --extract-audio --audio-format mp3 --cookies-from-browser chrome --add-header "Accept:*/*" -o "{}"'.format(sys.argv[1], sys.argv[2]))
